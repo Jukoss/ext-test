5b3f05cbf639d69b7fee5b6a1a04c2d12823c4b1
vasyl.tusiuk <email@example.com>
2019-06-24 17:04:44 +0300

    added sidebar

--- content.js
+++ content.js
@@ -1,13 +1,37 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
 
-    console.log('bla', request);
-    document.body.classList.add('bla');
+const body = document.body;
 
-    // if( request.message === "clicked_browser_action" ) {
-    //   var firstHref = document.querySelector[0].getAttribute("href");
+const sidebar = document.createElement("div");
+sidebar.classList.add('sidebar');
+body.classList.add('test-searching');
 
-    //   console.log('bla', firstHref);
-    // }
-  }
-);
+
+const mPortlet = document.createElement("div");
+mPortlet.classList.add('m-portlet');
+sidebar.append(mPortlet);
+
+const mPortletHead = document.createElement("div");
+mPortletHead.classList.add('m-portlet__head');
+mPortlet.append(mPortletHead);
+
+const mPortletBody = document.createElement("div");
+mPortletBody.classList.add('m-portlet__body');
+mPortlet.append(mPortletBody);
+
+const mPortletHeadCaption = document.createElement("div");
+mPortletHeadCaption.classList.add('m-portlet__head-caption');
+mPortletHead.append(mPortletHeadCaption);
+
+const mPortletHeadTitle = document.createElement("div");
+mPortletHeadTitle.classList.add('m-portlet__head-title');
+mPortletHeadCaption.append(mPortletHeadTitle);
+
+const mPortletHeadText = document.createElement("h3");
+mPortletHeadText.classList.add('m-portlet__head-text');
+mPortletHeadText.innerHTML = 'Search request';
+mPortletHeadTitle.append(mPortletHeadText);
+
+
+
+
+body.append(sidebar);
